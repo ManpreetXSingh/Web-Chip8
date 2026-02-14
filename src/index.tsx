c639e4dace14ab407c61942ee3d594732e88b60b
Manpreet Singh <63737630+ManpreetXSingh@users.noreply.github.com>
2026-02-14 21:00:41 +0530

    begin solidjs migration

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { render } from "solid-js/web";
+import "solid-devtools";
+
+const root = document.getElementById("root");
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
+  );
+}
+
+render(() => <div>Hello World</div>, root!);
